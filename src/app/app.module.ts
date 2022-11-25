49cf004e751c23c67840e5c2bed913fe3d8e5b9a
shajn786 <shajn786@gmail.com>
2022-11-25 18:30:47 +0530

    view all friend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FriendAddComponent } from './friend-add/friend-add.component';
+import { FriendViewAllComponent } from './friend-view-all/friend-view-all.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FriendAddComponent
+    FriendAddComponent,
+    FriendViewAllComponent
   ],
   imports: [
     BrowserModule,
