cc81ddd2059a70f3650ab7d661f2e27acd8ae4f2
shajn786 <shajn786@gmail.com>
2022-11-25 18:20:57 +0530

    add friend

---
+++ src/app/friend-add/friend-add.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-friend-add',
+  templateUrl: './friend-add.component.html',
+  styleUrls: ['./friend-add.component.css']
+})
+export class FriendAddComponent {
+
+}
